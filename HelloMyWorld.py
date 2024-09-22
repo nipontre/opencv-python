1374e7b69e1b0e505c2b012b33126f6850064ed7
razali <razali.mohamad@treasury.gov.my>
2024-09-23 00:39:37 +0800

    upd

---
+++ HelloMyWorld.py
@@ -0,0 +1,85 @@
+# print("Hello World")
+# print(2**3)
+# print(100000)
+# This print some text
+# print( "what's up" ) # This print some text
+# red_bucket = input('what do you what in the bucket?')
+
+# print(red_bucket)
+
+# print(5!=4)
+
+Thomas_age = 16
+age_at_tadika = 5
+
+#LOGIC
+# print(Thomas_age == age_at_tadika)
+
+# IF ELSE
+# if Thomas_age < age_at_tadika:
+#     print("preschool")
+# elif Thomas_age == age_at_tadika:
+#     print("enjoy kindergarden")
+# elif Thomas_age > age_at_tadika:
+#     print("High School")    
+# else:
+#     print(" other class")
+
+
+# Function
+# print("Thomas has a preschool")
+# print("Thomas has a preschool")
+# print("Thomas has a preschool")
+
+
+# def print_thomas():
+#     text = "Thomas as a Hight school student"
+#     print(text)
+#     print(text)
+#     print(text)
+
+# print_thomas()
+
+#2) 
+# def print_thomas(text):  
+#     print(text)
+#     print(text)
+#     print(text)
+
+# print_thomas('Thomas steven high school student')
+
+#3) 
+# def school_age_calc(age,name):
+#     if age < 5:
+#         print('enjoy', name, "is only", age )
+#     elif age == 5:
+#         print("enjod school", name)
+#     else:
+#         print("they adult")
+
+# school_age_calc(10, "Thomas")
+
+#4
+# def add_ten_to_age(age):
+#     new_age = age + 10
+#     return new_age
+
+# How_old_I_be = add_ten_to_age(3)
+# print(How_old_I_be)
+
+#LOOP - while and for
+# x=0
+# while(x<5):
+#     print(x)
+#     x=x+1
+
+
+# for x in range(5,10):
+#     print(x)
+
+# days=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+
+# for d in days:
+#     if(d=="Thu"):break
+#     # if(d=="Thu"):continue
+#     print(d)
